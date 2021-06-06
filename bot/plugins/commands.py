868a6fe0aacd38b5a468e93f94e9c17bdae65801
mikhael324 <83812749+mikhael324@users.noreply.github.com>
2021-06-06 10:22:40 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -24,7 +24,7 @@ async def start(bot, update):
         if (file_id or file_type) == None:
             return
         
-        caption = file_caption if file_caption != ("" or None) else ("<code>" + file_caption + "</code>")
+        caption = file_name if file_name != ("" or None) else ("<code>" + file_caption + "</code>")
         
         if file_type == "document":
         
