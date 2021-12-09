ab22ad7f1c7809b0db13c88f9e9740c27bef52bb
mikhael324 <83812749+mikhael324@users.noreply.github.com>
2021-12-09 23:59:22 +0530

    Update auto_filter.py

--- bot/plugins/auto_filter.py
+++ bot/plugins/auto_filter.py
@@ -207,7 +207,6 @@ async def auto_filter(bot, update):
                 reply_markup=reply_markup,
                 parse_mode="html",
                 reply_to_message_id=update.message_id
-                await asyncio.sleep(10)
                 await text.delete()
             )
 
