9eb87cbe72ea65c11ad9b240376733824751419c
mikhael324 <83812749+mikhael324@users.noreply.github.com>
2021-12-12 13:30:08 +0530

    Update auto_filter.py

--- bot/plugins/auto_filter.py
+++ bot/plugins/auto_filter.py
@@ -204,7 +204,7 @@ async def auto_filter(bot, update):
         try:
             await bot.send_message(
                 chat_id = update.chat.id,
-                message.reply_photo(https://images.app.goo.gl/Zbg3pd1cXYmtAHFW7)
+                message.reply_photo(https://images.app.goo.gl/Zbg3pd1cXYmtAHFW7),
                 text="""𝐇𝐞𝐲👋 𝐁𝐮𝐝𝐝𝐲😎 \n \n""" f"📁 𝐅𝐨𝐮𝐧𝐝 ✨ <b>{(len_results)}</b> 𝐅𝐢𝐥𝐞𝐬 𝐅𝐨𝐫 𝐘𝐨𝐮𝐫 𝐌𝐨𝐯𝐢𝐞: <b>{query}</b>👇",
                 reply_markup=reply_markup,
                 parse_mode="html",
